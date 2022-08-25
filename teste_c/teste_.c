4a912c6a78d8319747f9b72089829f2fe7effeeb
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-08-25 17:08:34 -0300

    Update teste_.c

--- teste_c/teste_.c
+++ teste_c/teste_.c
@@ -1,3 +1,24 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdbool.h>
+
+// int main() {
+//     char s[3] = "um";
+//     char t[4] = "ums";
+//     int conferir = true;
+    
+//     for(int i = 0; i < sizeof(s) || i < sizeof(t); i++){
+//         if(s[i] != t[i]){
+//             conferir = false;
+//         }
+//     }
+//     if(conferir)
+//       puts("iguais");
+//     else
+//       puts("Diferentes");
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -5,13 +26,15 @@
 int main() {
     char s[3] = "um";
     char t[4] = "ums";
+    int i;
     int conferir = true;
     
-    for(int i = 0; i < sizeof(s) || i < sizeof(t); i++){
+    for(i = 0; s[i] != '\0' || t[i] != '\0'; i++){
         if(s[i] != t[i]){
             conferir = false;
         }
     }
+    printf("%d", i);
     if(conferir)
       puts("iguais");
     else
