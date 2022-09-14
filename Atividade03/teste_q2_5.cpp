0eff373b981b44d5f7072a120b13e0d488088fee
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-14 20:31:23 -0300

    Update teste_q2_5.cpp

--- Atividade03/teste_q2_5.cpp
+++ Atividade03/teste_q2_5.cpp
@@ -1,62 +1,64 @@
 #include <iostream>
 #include <stdbool.h>
-#include "pilha_int_vetor.h"
+#include "pilha_char.h"
 using namespace std;
 
-bool conferir_caractere(int x,int y)
+bool conferir_caractere(char x, char y)
 {
     switch (x)
     {
-    case '(':
-      
-        if(y == ')')
-          return true;
+    case ')':
+
+        if (y == '(')
+            return true;
         else
-          return false;
+            return false;
         break;
-    case '[':
-        if(y == ']')
-          return true;
+    case ']':
+        if (y == '[')
+            return true;
         else
-          return false;
+            return false;
         break;
-    case '{':
-        if(y == '}')
-          return true;
+    case '}':
+        if (y == '{')
+            return true;
         else
-          return false;
+            return false;
         break;
     }
 }
 
-bool conferir_expressao(string x)
+bool conferir_expressao(string x, int tam)
 {
 
-    int y, z;
-    int k = x.length();
+    char y, z;
+    int k = tam;
     bool valor_cont = false;
-    Pilha *aux = pilha(x.length());
+    Pilha *aux = pilha();
 
-    while(k >= 0)
+    while (k >= 0)
     {
 
-        if (x.length() % 2 != 0)
+        if (tam % 2 != 0)
         {
             return false;
         }
 
-        if(x[k] == '(' || x[k] == '[' || x[k] == '{'){
-             empilha(x[k], aux);
-        }else if(x[k] == ')' || x[k] == ']' || x[k] == '}'){
+        if (x[k] == '(' || x[k] == '[' || x[k] == '{')
+        {
+            empilhar(&x[k], aux);
+        }
+        else if (x[k] == ')' || x[k] == ']' || x[k] == '}')
+        {
             z = desempilha_com_retorno(aux);
             valor_cont = conferir_caractere(x[k], z);
         }
 
-       
         if (!valor_cont)
             return false;
-       break;
-       k--;
+        break;
+        k--;
     }
     return true;
 }
@@ -64,16 +66,15 @@ bool conferir_expressao(string x)
 main()
 {
 
-    int x;
     //  cout << "Digite a qtd de caracteres que a expressao possui: ";
     // cin >> x;
 
     string expressao;
-    cout << "Digite a expressao a seguir (*sem espacos):\n=> ";
+    cout << "Digite a expressao a seguir :\n=> ";
     cin >> expressao;
-    //x = expressao.length();
+    int tam = expressao.length();
 
-    if (conferir_expressao(expressao))
+    if (conferir_expressao(expressao, tam))
         cout << "Expressao balanceada\n";
     else
         cout << "Expressao nao balanceada\n";
