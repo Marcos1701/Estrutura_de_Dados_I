99a5641b9e83feedb213be23b6de8b1ca248b638
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-14 21:29:14 -0300

    Update testeq_2_4_v1.cpp

--- Atividade03/testeq_2_4_v1.cpp
+++ Atividade03/testeq_2_4_v1.cpp
@@ -5,25 +5,38 @@ using namespace std;
 
 void organizar_pilha(Pilha *C, Pilha *D)
 {
-    // int aux;
-    bool valor_cont;
+    // bool valor_cont;
+    int k = C->topo->, aux;
+    cout << "k: " << k << endl;
 
-    for (int i = 0; i < C->topo; i++)
+    for (int i = 0; i < k; i++)
     {
-        valor_cont = false;
+        // valor_cont = false;
 
-        if (C->item[i] < C->item[i + 1])
+        for (int j = i + 1; j <= k; j++)
         {
-            // aux = A->item[i];
-            empilha(C->item[i], D);
-            C->item[i] = C->item[i + 1];
-            C->item[i + 1] = desempilha_com_retorno(D);
+
+            if (C->item[i] < C->item[j])
+            {
+                // aux = A->item[i];
+                empilha(C->item[i], D);
+                empilha(C->item[j], D);
+                desempilha(C);
+                desempilha(C);
+                empilha(desempilha_com_retorno(D), C);
+                empilha(desempilha_com_retorno(D), C);
+                // aux = desempilha_com_retorno(C);
+                // C->item[i - 1] = C->item[i];
+                // C->item[i] = desempilha_com_retorno(D);
+                cout << "conferindo: " << C->item[j] << endl;
+                // valor_cont = true;
+            }
         }
 
-        if (!valor_cont)
-        {
-            break;
-        }
+        // if (!valor_cont)
+        //{
+        //     break;
+        // }
     }
 
     imprimir_com_quebra_de_linha(C);
