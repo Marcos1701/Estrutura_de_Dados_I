bf75a0dd4fa524c7b917914e3d6dca4b328fa887
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-16 19:56:48 -0300

    Update teste_q_2_5v2.c

--- Atividade03/teste_q_2_5v2.c
+++ Atividade03/teste_q_2_5v2.c
@@ -1,91 +1,53 @@
-//#include <iostream>
-#include <stdbool.h>
 #include "pilha_caracter.h"
-// using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-bool conferir_caractere(char x, char y)
-{
-    switch (x)
-    {
-    case ')':
-
-        if (y == '(')
-            return true;
-        else
-            return false;
-        break;
-    case ']':
-        if (y == '[')
-            return true;
-        else
-            return false;
-        break;
-    case '}':
-        if (y == '{')
-            return true;
-        else
-            return false;
-        break;
-    }
-    return false;
-}
-
-bool conferir_expressao(char *x)
+int conferir_expressao(char x[])
 {
 
-    char z;
-    int k = 0;
-    bool valor_cont = false;
+    int k = strlen(x);
     Pilha *aux = pilha();
 
-    while (x[k] != '\0')
+    for (int i = 0; i <= k; i++)
     {
+        char z = x[i];
 
-        // if (tam % 2 != 0)
-        // {
-        //     return false;
-        // }
-
-        if (x[k] == '(' || x[k] == '[' || x[k] == '{')
+        if (x[i] == '(' || x[i] == '[' || x[i] == '{')
         {
-            empilhar(x[k], aux);
-            printf("v : %c\n", x[k]);
+            empilhar(x[i], aux);
+            printf("v : %c\n", x[i]);
         }
-        else if (x[k] == ')' || x[k] == ']' || x[k] == '{')
+        else if (x[i] == ')' || x[i] == ']' || x[i] == '{')
         {
+            if (vaziap(aux))
+            {
+                destroi(aux);
+                return 0;
+            }
             z = desempilha_com_retorno(aux);
-            valor_cont = conferir_caractere(x[k], z);
-            printf("v2 : %c\n", x[k]);
-        }
-        else
-        {
-            valor_cont = true;
-        }
 
-        if (!valor_cont)
-            return false;
-        break;
-        k++;
+            if ((x[i] == ')' && z != '(') || (x[i] == ']' && z != '[') || (x[i] == '{' && z != '}'))
+            {
+                return 0;
+            }
+            printf("v2 : %c\n", x[i]);
+        }
+        // k++;
     }
-    return true;
+    return 1;
 }
 
-main()
+int main()
 {
-
-    //  cout << "Digite a qtd de caracteres que a expressao possui: ";
-    // cin >> x;
 
     char expressao[50];
     printf("Digite a expressao a seguir :\n=> ");
     scanf("%s", expressao);
-    // getline(cin, expressao);
-    //  int tam = expressao.length();
 
-    // cout << "Tamanho: " << tam << endl
-    //      << "exp :" << expressao << endl;
     if (conferir_expressao(expressao))
         printf("Expressao balanceada\n");
     else
         printf("Expressao nao balanceada\n");
+
+    return 0;
 }
