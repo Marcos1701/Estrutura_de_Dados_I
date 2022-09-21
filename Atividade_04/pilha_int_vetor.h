2dee75259bec4d9733a9ed7f53ba280349295a79
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-20 21:07:46 -0300

    Add files via upload

---
+++ Atividade_04/pilha_int_vetor.h
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pilha
+{
+    int topo, max, tamanho = 0;
+    int *item;
+} Pilha;
+
+Pilha *pilha(int tam)
+{
+    Pilha *p = (Pilha *)malloc(sizeof(Pilha));
+    p->topo = -1;
+    p->item = (int *)malloc((tam) * sizeof(int));
+    p->max = tam;
+    return p;
+}
+
+void empilha(int valor, Pilha *p)
+{
+
+    if (p->topo == -1)
+    {
+        p->topo++;
+        p->tamanho++;
+        p->item[p->topo] = valor;
+    }
+    else if (p->topo == p->max)
+    {
+        printf("Pilha cheia");
+    }
+    else
+    {
+        p->topo++;
+        p->tamanho++;
+        p->item[p->topo] = valor;
+    }
+
+    printf("\nO valor: %d foi empilhado\n\n", valor);
+    return;
+}
+
+int desempilha(Pilha *p)
+{
+    int valor = p->item[p->topo];
+
+    if (p->topo == -1)
+    {
+        printf("\nA pilha esta vazia\n\n");
+    }
+    else
+    {
+        p->item[p->topo] = 0;
+        p->topo--;
+        p->tamanho--;
+        return valor;
+        // printf("\nO valor: %d foi desempilhado\n", valor);
+        // printf("Tamanho atual da pilha: %d\n\n", p->tamanho);
+    }
+    return valor;
+}
+
+void imprimir_com_quebra_de_linha(Pilha *p)
+{
+
+    int x = p->topo;
+    if (p->topo != -1)
+    {
+        while (x >= 0)
+        {
+            printf("=> %d \n", p->item[x]);
+            x--;
+        }
+    }
+    else
+    {
+        printf("A pilha esta vazia\n");
+    }
+    return;
+}
+
+void imprimir_sem_quebra_de_linha(Pilha *p)
+{
+    int x = 0;
+    if (p->topo != -1)
+    {
+        while (x <= p->tamanho)
+        {
+            printf("%d", p->item[x]);
+            x++;
+        }
+    }
+    else
+    {
+        printf("A pilha esta vazia\n");
+    }
+    return;
+}
+
+void destroi(Pilha *p)
+{
+    free(p);
+    p = NULL;
+    if (p == NULL)
+    {
+        printf("Pilha destruida");
+    }
+    return;
+}
