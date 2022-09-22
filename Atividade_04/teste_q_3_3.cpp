ee40242ebaa17ed64836bcd4efbcc3263251cdcc
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-22 16:44:57 -0300

    Create teste_q_3_3.cpp

---
+++ Atividade_04/teste_q_3_3.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include "pilha_int_vetor.h"
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+
+int prio(char o)
+{
+
+    if (o == '(')
+        return 0;
+    else if (o == '-' || o == '+')
+        return 1;
+    else if (o == '*' || o == '/')
+        return 2;
+    return 0; // operador invalido!
+}
+
+char *gerar_sufixa(char *inf)
+{
+    static char s[250];
+    char *x;
+    int k = 0;
+    x = inf;
+
+    Pilha *p = pilha(strlen(inf));
+
+    for (int i = 0; *x != '\0'; i++)
+    {
+        if (isalnum(*x))
+        {
+            cout << "valor emp: " << *x << endl;
+            s[k++] = *x;
+            s[k++] = ' ';
+        }
+        else if (*x == '(')
+        {
+            empilha(*x, p);
+        }
+        else if (*x == ')')
+        {
+            while (p->item[p->topo] != '(')
+            {
+                s[k++] = desempilha(p);
+                s[k++] = ' ';
+            }
+            desempilha(p);
+        }
+        else
+        {
+            while (prio(p->item[p->topo]) >= prio(*x))
+            {
+                s[k++] = desempilha(p);
+                s[k++] = ' ';
+            }
+            empilha(*x, p);
+            cout << "valor emp: " << *x << endl;
+        }
+        x++;
+    }
+    while (p->topo != -1)
+    {
+        s[k++] = desempilha(p);
+        s[k++] = ' ';
+    }
+    s[k++] = '\0';
+    cout << "s: " << s << endl;
+    destroi(p);
+    return s;
+}
+
+int main()
+{
+
+    char infixa[150];
+
+    cout << "Digite a expressao infixa a seguir: " << endl
+         << "=> ";
+    cin >> infixa;
+
+    char *sufixa;
+    sufixa = gerar_sufixa(infixa);
+    
+    int valor = valor(sufixa);
+    //(sufixa, gerar_sufixa(infixa));
+
+    cout << "\nInfixa digitada: " << infixa << endl;
+    cout << "Sufixa resultante: " << sufixa << endl;
+
+
+    return 0;
+}
