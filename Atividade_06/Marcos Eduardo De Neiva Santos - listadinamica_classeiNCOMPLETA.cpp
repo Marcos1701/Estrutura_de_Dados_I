da8e57ded818a9b25828257ddbc58d2e37a3b28c
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-10-11 17:21:30 -0300

    Add files via upload

---
+++ Atividade_06/Marcos Eduardo De Neiva Santos - listadinamica_classeiNCOMPLETA.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+class No{
+	public:
+		int mat;
+		char nome[23];
+		No *prox;
+		No(int m,char n[23]){
+			mat=m;
+			strcpy(nome,n);			
+			prox=NULL;
+		}
+};
+
+class Lista{
+	public:
+		No *inicio;
+		No *fim;
+	
+	    Lista(){
+	    	inicio = NULL;
+	    	fim = NULL;
+		}	
+		void addToFinal(int m, char n[23]){
+			No *novo= new No(m,n);
+			if (fim==NULL){
+				inicio = novo;
+				fim = novo;
+			}
+			else{
+				fim->prox = novo;
+				fim=novo;
+			}
+		}
+		
+			
+		int lst_vazia(){
+			return (inicio==NULL);
+		}
+
+		void addToInicio(int m,char n[23]){
+		
+		No *novo = new No(m, n);
+		   if(inicio == NULL){
+			inicio = novo;
+		   }else{
+			novo->prox = inicio;
+			inicio = novo;
+		   }
+		   return;
+		}
+
+		void mostra(){
+			cout << endl << "-------- Mostra --------" << endl;
+
+			if (lst_vazia()){
+				cout<<"\nLista vazia!!!";
+			}
+			else{
+				No *aux = inicio;
+				for(int i = 0; aux != NULL; aux = aux->prox){
+					cout << endl << "Mat: " << aux ->mat << endl << "Nome : " << aux ->nome << endl;
+					i++;
+				}
+				cout << endl;
+			}
+		}
+						
+		
+		int remove(int mat){
+			 
+			No *aluno = busca(mat);
+
+			if(aluno == NULL){
+               //cout << "Matricula nao encontrada!!!" << endl;
+			   return 1;
+			}
+			No *aux = aluno->prox;
+             
+			aluno ->prox = aux->prox;
+			free(aux);
+            //cout << "Aluno removido com sucesso" << endl;
+			return 0;
+		}
+		
+		// auxiliar no metodo remove, vai 
+		//retornar o endereco do anterior
+		No *busca(int mat){
+			if (lst_vazia()){
+				cout<<"\nErro, Lista vazia!!!";
+			}
+			else{
+				No *aux = inicio;
+
+				while(aux != NULL){
+					if(aux->prox->mat == mat){
+                        break;
+					}
+					aux = aux->prox;
+					
+				}
+					return aux;
+				
+			}
+		}
+		
+		void removeTodos(){
+			No *atual = inicio;
+			No *proximo = NULL;
+			while (atual != NULL){
+					proximo = atual->prox;
+					cout << "\n Apaga: " << atual->nome;
+					free(atual);
+					atual = proximo;						
+			}
+			inicio = atual;
+		}
+		
+		// criar uma nova lista que seja o inverso da primeira
+		void crialistainversa(){
+			
+			Lista *lista = new Lista();
+            No *aux = inicio;
+			No *aux2 = fim;
+
+			while(aux != aux2){
+				lista->addToInicio(aux->mat,aux->nome);
+				aux = aux->prox;
+			}
+			return;
+		}
+		
+		//inverter a propria lista
+		void listainvertida(){
+            No *aux = inicio;
+
+			No *aux2 = fim;
+
+			do{
+				addToFinal(aux->mat,aux->nome);
+				aux = aux->prox;
+				free(inicio);
+				inicio = aux;
+			}while(aux != aux2);
+			
+			return;
+		}
+};
+
+int main(){
+
+   Lista *l = new Lista();
+   l->addToFinal(1,"Joao");
+   l->addToFinal(2,"Gabriela");
+   l->addToFinal(3,"Fabiola");
+   l->addToInicio(5,"Joana Oliveira");
+   l->addToInicio(6,"Cristiano Oliveira");
+   l->mostra();
+ 
+  l->listainvertida();
+  l->mostra();
+
+   return 0;
+
+}
