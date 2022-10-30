0557ab8d3d4ea3122f37c50337bc7fb7067da020
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-10-30 11:08:27 -0300

    Update Q7.cpp

--- Atividade_cap_10/Q7.cpp
+++ Atividade_cap_10/Q7.cpp
@@ -3,27 +3,69 @@
 #include <iostream>
 using namespace std;
 
+void adt(float c, int e, Poli *P)
+{
 
-void adt(float c, int e, Poli *P){
-
- if( *P == NULL || (*P)->expo < e){
+    if (*P == NULL || (*P)->expo < e)
+    {
         *P = termo(c, e, *P);
-    }else if((*P)->expo == e){
+    }
+    else if ((*P)->expo == e)
+    {
         (*P)->coef += c;
-        if((*P)->coef == 0){
+        if ((*P)->coef == 0)
+        {
             Poli aux = *P;
             *P = aux->prox;
             free(aux);
         }
-    }else adt(c, e, &(*P)->prox);
+    }
+    else
+        adt(c, e, &(*P)->prox);
 }
 
-int main(){
-   
-   Poli p = NULL;
-   adt(+1, 0, &p);
-   adt(-6, 2, &p);
-   adt(+3, 5, &p);
-   exibeop(p);
-   
+Poli soma(Poli P, Poli Q)
+{
+    Poli retorno = NULL;
+    Poli aux = P;
+
+    while (aux)
+    {
+        retorno = termo(aux->coef, aux->expo, retorno);
+        aux = aux->prox;
+    }
+    aux = Q;
+    while (aux)
+    {
+        adt(aux->coef, aux->expo, &retorno);
+        aux = aux->prox;
+    }
+    return retorno;
+}
+
+int main()
+{
+
+    Poli p = NULL;
+    Poli q = NULL;
+    adt(+1, 0, &p);
+    adt(-6, 2, &p);
+    adt(+3, 5, &p);
+
+    adt(+1, 0, &q);
+    adt(-6, 2, &q);
+    adt(+3, 5, &q);
+    cout << "----- Poli P -----" << endl;
+    exibeop(p);
+    puts("\n");
+    cout << "----- Poli Q -----" << endl;
+
+    exibeop(q);
+    puts("\n");
+
+    cout << "----- Soma P e Q -----" << endl;
+
+    exibeop(soma(p, q));
+    puts("\n");
+    // exibeop(p);
 }
