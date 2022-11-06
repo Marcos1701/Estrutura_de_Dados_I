c40aba0238b8838a03bca39d10738bc30f8fabe9
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-05 22:24:02 -0300

    Update Marcos Eduardo De Neiva Santos - QueueCompletar.cpp

--- Atividade_stack_and_queue/Marcos Eduardo De Neiva Santos - QueueCompletar.cpp
+++ Atividade_stack_and_queue/Marcos Eduardo De Neiva Santos - QueueCompletar.cpp
@@ -3,44 +3,57 @@
 using namespace std;
 
 template <class T>
-class Queue: public queue<T> {
-	protected:
-		queue<T> fila;
-	public:
-	T popQueue(){
-		T tmp=fila.front();
-		fila.pop();
-		return tmp;
-	}
-	
-	
-	void popTodos(){				
-	   
-       for(int i = 0; i < fila.size(); i++){
-           fila.pop();
-       }
+class Queue : public queue<T>
+{
+protected:
+    queue<T> fila;
+
+public:
+    T popQueue()
+    {
+        T tmp = fila.front();
+        fila.pop();
+        return tmp;
     }
-    
-    void simulaPilha(T novo){
+
+    void popTodos()
+    {
+
+        while (!fila.empty())
+        {
+            fila.pop();
+        }
+    }
+
+    void simulaPilha(T novo)
+    {
         queue<T> aux;
 
-        for(int i = 0; i < fila.size(); i++){
-            aux.push(fila[i]);
+        while (!fila.empty())
+        {
+            aux.push(fila.front());
+            fila.pop();
         }
-        fila.popTodos();
-        for(int i = 0; i < fila.size(); i++)
-           fila.push(aux.pop());
 
-        free(aux);
+        fila.push(novo);
+        while (!aux.empty())
+        {
+            fila.push(aux.front());
+            aux.pop();
+        }
     }
 };
 
-main(){
-	Queue<int> fila;
-	fila.push(4);
-	fila.push(5);
-    fila.simulaPilha();
-	cout<<fila.front()<<endl;
-	
+main()
+{
+    Queue<int> fila;
+    fila.push(4);
+    fila.push(5);
+    fila.simulaPilha(3);
 
+    while (!fila.empty())
+    {
+        cout << "=> " << fila.front() << endl;
+        fila.pop();
+    }
 }
