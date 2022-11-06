4ca439e1727224b325fecb0c44e6631568ed01a7
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-05 22:23:31 -0300

    Update Marcos Eduardo De Neiva Santos - StackCompletar.cpp

--- Atividade_stack_and_queue/Marcos Eduardo De Neiva Santos - StackCompletar.cpp
+++ Atividade_stack_and_queue/Marcos Eduardo De Neiva Santos - StackCompletar.cpp
@@ -3,62 +3,62 @@
 using namespace std;
 
 template <class T>
-class Stack: public stack<T> {
-	protected:
-		stack<T> pilha;
-	public:
-	T popStack(){
-		T tmp=pilha.top();
-		pilha.pop();
-		return tmp;
-	}
-	
-	
-	void popTodos(){
-		for(int i = 0; i < pilha.size(); i++){
+class Stack : public stack<T>
+{
+protected:
+    stack<T> pilha;
+
+public:
+    T popStack()
+    {
+        T tmp = pilha.top();
+        pilha.pop();
+        return tmp;
+    }
+
+    void popTodos()
+    {
+        while (!pilha.empty())
+        {
             cout << "=> " << pilha.top() << " removido" << endl;
             pilha.pop();
         }
+        return;
     }
-	
-	void simulaFila(T novo){
-        
-       int valores[pilha.size()];
-        for(int i = pilha.size() - 1; i >= 0; i++){
-            valores[i] = pilha.top();
+
+    void simulaFila(T novo)
+    {
+
+        stack<T> aux;
+
+        while (!pilha.empty())
+        {
+            cout << pilha.top() << endl;
+            aux.push(pilha.top());
             pilha.pop();
         }
-
-        for(int i = 0; i < pilha.size(); i++){
-          pilha.push(valores[i]);
-        }
-	}
-	
-	
+        aux.push(novo);
+        pilha = aux;
+    }
 };
 
-main(){
-	
-	Stack<int> teste;
+int main()
+
+{
+
+    Stack<int> teste;
 
     teste.push(1);
     teste.push(2);
-
-    cout << "=> " << teste.top() << endl;
-    teste.popTodos();
-    teste.push(0);
-    cout << "=> " << teste.top() << endl;
-
-    teste.push(12);
-    teste.push(122);
-
+    teste.push(3);
+    teste.push(4);
+    teste.push(5);
     teste.simulaFila(1);
-    cout << "=> " << teste.top() << endl;
 
-    cout << "=> " << teste.top() << endl;
-    teste.pop();
-    cout << "=> " << teste.top() << endl;
-    teste.pop();
-    cout << "=> " << teste.top() << endl;
-
+    while (!teste.empty())
+    {
+        cout << teste.top() << endl;
+        teste.pop();
+    }
+    return 0;
 }
