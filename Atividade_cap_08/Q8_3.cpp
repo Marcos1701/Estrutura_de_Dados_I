c63c365ccf55eeee2e40aab964ea7d230c9a19dc
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-10 17:06:10 -0300

    Update Q8_3.cpp

--- Atividade_cap_08/Q8_3.cpp
+++ Atividade_cap_08/Q8_3.cpp
@@ -1,18 +1,29 @@
-// Usando a função empurra (), 
-//que move o item máximo de um vetor 
-//para sua última
-// posição, crie a função recursiva bubbleSort (v,n),
-// que ordena um vetor V com n
-// números inteiros.
-// void empurra (int v[], int n) {
-//    for (int i=0; i<n; i++)
-//       if( v[i]>v[i+1])
-//           troca (v[i],v[i+11]);
-// }
+
 
 #include <iostream>
 
 using namespace std;
+
+//void bubble_sort(double v[], int tam){
+//    int aux = tam;
+
+//    for(int i = 0; i < tam; i++){
+//       int cont = 0;
+//       for(int j = 0; j < aux; j++){
+//          if(v[j]>v[j+1]){
+//            double x = v[j];
+//            v[j] = v[j+1];
+//            v[j+1] = x;
+//            cont = 1;
+//          }
+//       }
+//       if(!cont){
+//          break;
+//       }
+//    }
+// }
+
+//ou..
 
 #define troca(a, b) { int x = a; a = b; b = x;};
 void empurra (int v[], int n) {
