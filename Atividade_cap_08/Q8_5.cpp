4e974c439918a6b005e22dab164b2313a65466f1
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-15 20:13:16 -0300

    Create Q8_5.cpp

---
+++ Atividade_cap_08/Q8_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int v[], int n)
+{
+    if (n <= 1)
+        return;
+
+    insertionSort(v, n - 1);
+
+    int anterior = v[n - 1];
+    int k = n - 2;
+
+    while (k >= 0 && v[k] > anterior)
+    {
+        v[k + 1] = v[k];
+        k--;
+    }
+
+    v[k + 1] = anterior;
+}
+
+int main()
+{
+    int v[] = {1, 4, 2, 3, 6, 7, 5, 0, -1, 10};
+
+    insertionSort(v, 9);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
